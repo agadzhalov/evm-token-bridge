e49618cf9719f156ee4582c57b4ba684b591c2b0
--replace-all <al.gadzhalov@gmail.com>
2022-11-26 13:40:06 +0200

    Changed: goerli deployment and verify scripts for erc20 and ethbridge

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,11 @@ import "@nomicfoundation/hardhat-toolbox";
 import interactLocalhost from "./scripts/interact-localhost";
 import interactPolygonLocalhost from "./scripts/interact-polygon-local";
 import deployInit from "./scripts/deploy-init";
+import deployERC20TokenGoerli from "./scripts/deploy-erc20-goerli";
+
+import * as dotenv from "dotenv";
+import deployEthereumBridgeGoerli from "./scripts/deploy-ethbridge-goerli";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -18,9 +23,29 @@ const config: HardhatUserConfig = {
       },
     }
   },
+  networks: {
+    goerli: {
+      url: process.env.RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : []
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY || ""
+  }
 };
 
 export default config;
+
+task("deploy-erc20-goerli", "Deploys EthereumToken, EthereumBrdige and PolygonBridge")
+  .setAction(async (args: any, hre: HardhatRuntimeEnvironment) => {
+    await deployERC20TokenGoerli(args, hre);
+  });
+
+task("deploy-ethbridge-goerli", "Deploys EthereumToken, EthereumBrdige and PolygonBridge")
+  .setAction(async (args: any, hre: HardhatRuntimeEnvironment) => {
+    await deployEthereumBridgeGoerli(args, hre);
+  });
+
 
 task("deploy-init", "Deploys EthereumToken, EthereumBrdige and PolygonBridge")
   .setAction(async (args: any, hre: HardhatRuntimeEnvironment) => {
